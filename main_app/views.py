82daf1bd5b10318f33f8166243eb00ef84d8794c
Lilish7295 <leylishams7831@gmail.com>
2023-12-27 15:19:07 +0330

    updte: django admin panel customized

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from works.models import Flight
 
-# Create your views here.
+
+def index(request):
+    if request.method == 'POST':
+        origin = request.POST['origin']
+        dest = request.POST['dest']
+        date = request.POST['date']
+        seat = request.POST['seat']
+        flights = Flight.objects.filter(origin__city=origin, destination__city=dest)
+        f_list ={
+            'flights' : flights
+        }
+        return render(request, 'works/list.html', context=f_list)
+    return render(request, 'main_app/index.html')
--- works/views.py
+++ works/views.py
@@ -28,3 +28,40 @@ def company(request):
 def home(request):
     return render(request, 'home.html')
 
+
+def detail(request, job_name):
+    if request.method == 'GET':
+        try:
+            jobs = Job.objects.get(title=job_name)
+        except:
+            jobs = None
+        j_list = {
+            'jobs' : jobs, 
+            'flag' : False
+        }
+        return render(request, 'works/detail.html', context=j_list)
+    if request.method == 'POST':
+        current_job = Job.objects.get(title=job_name)
+        description = request.POST['description']
+        category = request.POST['category']
+        location = request.POST['location']
+        employer = request.POST['employer']
+        created_at = request.POST['created_at']
+        min_salary = request.post['min_salary']
+        max_salary = request.post['max_salary']
+
+
+        Job.objects.create(
+            job = current_job,
+            description = description,
+            category = category,
+            location = location,
+            employer = employer,
+            created_at = created_at,
+            min_salary = min_salary,
+            max_salary = max_salary
+        )
+        
+        
+        return render(request, 'works/detail.html', context=j_list)
+
