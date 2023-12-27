7389ae5c4b706f0e74e3dc1ab45693d4ede61e0a
Lilish7295 <leylishams7831@gmail.com>
2023-12-27 15:46:08 +0330

    update:django admin panel customized

--- main_app/views.py
+++ main_app/views.py
@@ -1,16 +1,25 @@
 from django.shortcuts import render
-from works.models import Flight
+from works.models import Job
 
 
-def index(request):
+def index(request, job_name):
+    
     if request.method == 'POST':
-        origin = request.POST['origin']
-        dest = request.POST['dest']
-        date = request.POST['date']
-        seat = request.POST['seat']
-        flights = Flight.objects.filter(origin__city=origin, destination__city=dest)
-        f_list ={
-            'flights' : flights
+        current_job = Job.objects.get(title=job_name)
+        description = request.POST['description']
+        category = request.POST['category']
+        location = request.POST['location']
+        employer = request.POST['employer']
+        created_at = request.POST['created_at']
+        min_salary = request.post['min_salary']
+        max_salary = request.post['max_salary']
+        
+        jobs = Job.objects.filter(location_province=location,job_category=category)
+
+        t_list ={
+            'jobs' : jobs
         }
-        return render(request, 'works/list.html', context=f_list)
+
+        return render(request, 'works/list.html', context=t_list)
+    
     return render(request, 'main_app/index.html')
