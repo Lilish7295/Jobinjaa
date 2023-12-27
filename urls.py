a5dd23bec36d9ffcf4377b6f20766666b033696e
Sara4D <154618716+Sara4D@users.noreply.github.com>
2023-12-27 19:32:17 +0330

    Add files via upload

---
+++ urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import landing , home
+
+app_name = 'works'
+
+urlpatterns = [
+    path('', home, name='home'),
+    path('jobs/', landing, name='jobs'),
+    
+]
